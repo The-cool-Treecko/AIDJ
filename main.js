214e4741316ebfba9074f492d98bbe222fb8c251
The cool Treecko <77108834+The-cool-Treecko@users.noreply.github.com>
2022-01-18 17:50:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 var song;
+var leftX = 0;
+var rightX = 0;
+var leftY = 0;
+var rightY = 0;
 
 function preload() {
     song = loadSound("music.mp3");
@@ -9,7 +13,7 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
-    posenet = ml5.poseNet(canvas, modelLoaded);
+    posenet = ml5.poseNet(video, modelLoaded);
     posenet.on('pose', gotPoses);
 }
 
@@ -17,16 +21,24 @@ function modelLoaded() {
     console.log("model is loaded");
 }
 
-function gotPoses(results) {
-    if (results.length > 0) {
-        console.log(results);
-    }
-}
-
 function draw() {
     image(video,0,0,600,500);
 }
 
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftX = results[0].pose.leftWrist.x;
+        rightX = results[0].pose.rightWrist.x;
+        leftY = results[0].pose.leftWrist.y;
+        rightY = results[0].pose.rightWrist.y;
+        console.log("left wrist position = " + leftX + ", " + leftY);
+        console.log("right wrist position = " + rightX + ", " + rightY)
+    }
+}
+
 function play() {
+    song.setVolume(1);
+    song.rate(1);
     song.play();
 }
