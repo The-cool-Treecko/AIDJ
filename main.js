b61faabc03ed856ed1c4d6b4689c806368fff0d1
The cool Treecko <77108834+The-cool-Treecko@users.noreply.github.com>
2022-01-18 17:31:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,6 +9,18 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    posenet = ml5.poseNet(canvas, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("model is loaded");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
 }
 
 function draw() {
