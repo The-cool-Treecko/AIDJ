6b597b0a3ec7ced087cc07d8f9ab8fc8bd5923a0
The cool Treecko <77108834+The-cool-Treecko@users.noreply.github.com>
2022-01-20 17:52:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ var leftX = 0;
 var rightX = 0;
 var leftY = 0;
 var rightY = 0;
+var volume = 1;
+var leftScore = 0;
 
 function preload() {
     song = loadSound("music.mp3");
 }
 
 function setup() {
-    canvas = createCanvas(600,500);
+    canvas = createCanvas(600, 500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
@@ -22,7 +24,16 @@ function modelLoaded() {
 }
 
 function draw() {
-    image(video,0,0,600,500);
+    image(video, 0, 0, 600, 500);
+    stroke("#FF0000");
+    fill("#FF0000");
+    if (leftScore > 25) {
+        circle(leftX, leftY, 20);
+        document.getElementById("volume").innerHTML = "Volume: " + volume;
+        isNumberleftY = floor(Number(leftY));
+        volume = isNumberleftY / 500;
+        song.setVolume(volume);
+    }
 }
 
 function gotPoses(results) {
@@ -34,11 +45,13 @@ function gotPoses(results) {
         rightY = results[0].pose.rightWrist.y;
         console.log("left wrist position = " + leftX + ", " + leftY);
         console.log("right wrist position = " + rightX + ", " + rightY)
+        leftScore = results[0].pose.keypoints[9].score;
+        leftScore = (leftScore * 100).toFixed(2);
+
     }
 }
 
 function play() {
-    song.setVolume(1);
     song.rate(1);
     song.play();
 }
